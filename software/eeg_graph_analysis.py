a5bae1641791ca2186018d084e733c1147757df1
RJTK <ryan@kinnear.ca>
2019-07-07 19:12:40 -0400

    SVC classifier does not discriminate between A and C eeg networks.

--- software/eeg_graph_analysis.py
+++ software/eeg_graph_analysis.py
@@ -13,9 +13,13 @@ from eeg_analysis import channels
 
 from sklearn.svm import SVC
 from sklearn.model_selection import (StratifiedShuffleSplit, StratifiedKFold,
-                                     GridSearchCV)
+                                     GridSearchCV, learning_curve)
 # from sklearn.learning_curves import learning_curve
 from sklearn.metrics import matthews_corrcoef
+from sklearn.decomposition import PCA
+from sklearn.cross_decomposition import PLSRegression
+from sklearn.manifold import Isomap, TSNE
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
 from matplotlib import rc as mpl_rc
 font = {"family": "normal",
@@ -23,60 +27,65 @@ font = {"family": "normal",
         "size": 22}
 
 mpl_rc("font", **font)
-mpl_rc("text", usetex=True)
+mpl_rc("text", usetex=False)
 
 
 def main():
-    dataset = load_dataset()
+    dataset = load_dataset(avg_subjects=True)
 
     X = np.array(dataset["A"] + dataset["C"])
     y = np.append(np.ones(len(dataset["A"])),
                   np.zeros(len(dataset["C"])))
-    ss = StratifiedShuffleSplit(n_splits=1, test_size=0.75)
+    ss = StratifiedShuffleSplit(n_splits=1, test_size=0.1)
     train_ix, valid_ix = next(ss.split(X, y))
     X_valid, y_valid = X[valid_ix], y[valid_ix]
     X, y = X[train_ix], y[train_ix]
 
-    K_rk_apx = 20
+    K_rk_apx = False
     gk = GraphKernel(
         kernel=[{"name": "weisfeiler_lehman", "niter": 5},
-                {"name": "subtree_wl"}], normalize=True,
+                # {"name": "subtree_wl"},
+                {"name": "propagation"}
+                # {"name": "shortest_path", "with_labels": True}
+                ],
+        normalize=True,
         Nystroem=K_rk_apx,
         n_jobs=4)
 
     K = gk.fit_transform(X)
-    param_grid = {"C": np.logspace(-5, 5, 25)}
-    cv = GridSearchCV(estimator=SVC(kernel="linear"),
+
+    # lda = LinearDiscriminantAnalysis(n_components=3)
+    # K_lda = lda.fit_transform(K, y)
+
+    iso = Isomap(n_components=2, n_neighbors=5)
+    K_iso = iso.fit_transform(K)
+    plt.scatter(K_iso[:, 0], K_iso[:, 1], c=y)
+    plt.show()
+
+    tsne = TSNE(n_components=2)
+    K_tsne = tsne.fit_transform(K)
+    plt.scatter(K_tsne[:, 0], K_tsne[:, 1], c=y)
+    plt.show()
+
+    param_grid = {"C": np.logspace(-6, 1, 35),
+                  "gamma": np.logspace(-6, 1, 35)}
+    cv = GridSearchCV(estimator=SVC(kernel="rbf"),
                       param_grid=param_grid,
                       n_jobs=1, iid=True, cv=3,
                       refit=True)
     cv.fit(K, y)
 
-    # skf = StratifiedKFold(n_splits=3, shuffle=True)
-    # C_star = None
-    # score_star = -np.inf
-    # for c in np.logspace(-5, 5, 25):
-    #     total_score = 0
-    #     for train_ix, test_ix in skf.split(X, y):
-    #         K_train = gk.fit_transform(X[train_ix])
-    #         K_test = gk.transform(X[test_ix])
-
-    #         clf = SVC(C=c, kernel="percomputed", probability=False)
-    #         clf.fit(K_train, y[train_ix])
-    #         pred = clf.predict(K_test)
-    #         total_score += matthews_corrcoef(y[test_ix], pred)
-
-    #     if total_score > score_star:
-    #         score_star = total_score
-    #         C_star = clf.C
-
-    # K = gk.fit_transform(X)
-    # clf = SVC(C=C_star, kernel="percomputed", probability=True)
-    # clf.fit(K, y)
+    plot_learning_curve(cv.best_estimator_, "Learning Curves SVC",
+                        K, y, cv=10)
+    plt.show()
+    K_valid = gk.transform(X_valid)
+    y_hat_valid = cv.predict(K_valid)
+    acc_valid = np.sum((y_valid == y_hat_valid) / len(y_valid))
+    print(acc_valid)
     return
 
 
-def load_dataset():
+def load_dataset(avg_subjects=False):
     dataset = {"A": [], "C": []}
     # labels = {chan: chan for chan in channels}
     vertex_labels = {i: chan for i, chan in enumerate(channels)}
@@ -90,17 +99,44 @@ def load_dataset():
         else:
             continue
 
-        for adj_file in os.listdir(adj_mat_folder + folder):
-            if adj_file[-3:] != "npy":
-                continue
-            try:
-                file_name = os.path.join(adj_mat_folder, folder, adj_file)
-                adj = np.load(file_name)
-                adj = sparse_matrix(adj)
-                # g = adj_mat_to_graph(adj)
-                dataset[label].append([adj, vertex_labels])
-            except Exception as e:
-                print("Caught Exception {}, continuing...".format(e))
+        if avg_subjects:
+            dataset = _load_avg_graphs(folder, adj_mat_folder, dataset,
+                                       label, vertex_labels)
+        else:
+            dataset = _load_all_graphs(folder, adj_mat_folder, dataset,
+                                       label, vertex_labels)
+    return dataset
+
+
+def _load_avg_graphs(folder, adj_mat_folder, dataset,
+                     label, vertex_labels):
+    _dataset = {label: []}
+    _dataset = _load_all_graphs(folder, adj_mat_folder, _dataset,
+                                label, vertex_labels)
+    n_nodes = len(vertex_labels)
+    g = np.zeros((n_nodes, n_nodes))
+
+    for adj, _ in _dataset[label]:
+        adj = np.array(adj.todense())
+        g = g + adj
+    g /= n_nodes
+    dataset[label].append([g, vertex_labels])
+    return dataset
+
+
+def _load_all_graphs(folder, adj_mat_folder, dataset,
+                     label, vertex_labels):
+    for adj_file in os.listdir(adj_mat_folder + folder):
+        if adj_file[-3:] != "npy":
+            continue
+        try:
+            file_name = os.path.join(adj_mat_folder, folder, adj_file)
+            adj = np.load(file_name)
+            adj = sparse_matrix(adj)
+            # g = adj_mat_to_graph(adj)
+            dataset[label].append([adj, vertex_labels])
+        except Exception as e:
+            print("Caught Exception {}, continuing...".format(e))
     return dataset
 
 
@@ -111,3 +147,33 @@ def adj_mat_to_graph(adj):
             if adj[i, j]:
                 g[chan_from].append(chan_to)
     return g
+
+
+def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None,
+                        n_jobs=None, train_sizes=np.linspace(.1, 1.0, 5)):
+    plt.figure()
+    plt.title(title)
+    if ylim is not None:
+        plt.ylim(*ylim)
+    plt.xlabel("Training examples")
+    plt.ylabel("Score")
+    train_sizes, train_scores, test_scores = learning_curve(
+        estimator, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    plt.grid()
+
+    plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.1,
+                     color="r")
+    plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                     test_scores_mean + test_scores_std, alpha=0.1, color="g")
+    plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
+             label="Training score")
+    plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
+             label="Cross-validation score")
+
+    plt.legend(loc="best")
+    return plt
